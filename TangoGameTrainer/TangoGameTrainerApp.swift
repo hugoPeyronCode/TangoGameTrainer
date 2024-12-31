19ab0c6b97574292a48d50ae2e1749c59ee513a9
hugoPeyronCode <110022126+hugoPeyronCode@users.noreply.github.com>
2024-12-31 15:24:48 +0100

    Initial Commit

---
+++ TangoGameTrainer/TangoGameTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangoGameTrainerApp.swift
+//  TangoGameTrainer
+//
+//  Created by Hugo Peyron on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TangoGameTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
